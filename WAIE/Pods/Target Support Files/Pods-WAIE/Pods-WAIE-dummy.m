d2813021f2b8dcbacbaddca88ea00bf81364c65b
anshu <>
2023-02-09 22:50:55 +0530

    Initial Project setup for WaIE

---
+++ WAIE/Pods/Target Support Files/Pods-WAIE/Pods-WAIE-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WAIE : NSObject
+@end
+@implementation PodsDummy_Pods_WAIE
+@end
---
+++ WAIE/WAIE/WAIEApp.swift
@@ -0,0 +1,17 @@
+//
+//  WAIEApp.swift
+//  WAIE
+//
+//  Created by Anshu Kumar Ray on 09/02/23.
+//
+
+import SwiftUI
+
+@main
+struct WAIEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
